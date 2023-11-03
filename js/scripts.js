bc3f12a03dcb96a5b32903998973d83c1e22ad82
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2023-11-03 10:30:25 -0700

    function now returns proper response when welcome home button is selected

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 // Business Logic
 
 function beepBoop(number) {
-  const numArray = number.toString().split('')
+  const numArray = number.toString().split('');
   if (numArray.includes('3')) {
-    return "Won't you be my neighbor?"
+    return "Won't you be my neighbor?";
   } else if (numArray.includes('2')) {
-    return "Boop!"
+    return "Boop!";
   } else if (numArray.includes('1')) {
-    return "Beep!"
+    return "Beep!";
   } else {
     return number;
   }
-};
+}
 
 function pickANum(inputNumber) {
   const output = [];
@@ -19,53 +19,27 @@ function pickANum(inputNumber) {
     output.push(beepBoop(i));
   }
   return output;
-};
+}
 
 
-// function pickANum(number) {
-//   const output = [];
-//   let i = 0;
-//   while (i <= number) {
-//     output.push(i)
-//     i++
-//   }
-//   return output;
-// };
-
-// function beepBoop(number) {
-//   const numArray = number.toString().split(" ")
-//   if (numArray.includes("1")) {
-//     return "Beep!"
-//   }
-//   else if (numArray.includes("2")) {
-//     return "Boop!"
-//   }
-//   else if (numArray.includes("3")) {
-//     return "Won't you be my neighbor?"
-//   }
-// };
-
-// function numReplacer(array) {
-//   const array = array.map((number) => beepBoop(number));
-//   return numReplacer;
-// };
 
 // UI Logic
 
 function handleFormSubmission(e) {
   e.preventDefault();
-    const number = parseInt(document.getElementById("numSelect").value);
-    const result = document.getElementById("result");
-    const p = document.createElement("p");
-    const numResult = output(number);
-    p.append(numResult);
-    result.append(p);
-  }
+  const number = parseInt(document.getElementById("numSelect").value);
+  const result = document.getElementById("result");
+  result.innerHTML = "";
+  const p = document.createElement("p");
+  const numResult = pickANum(number);
+  p.textContent = numResult.join(", "); 
+  result.appendChild(p);
+}
 
-  window.addEventListener("load", function() {
-    const form = document.querySelector("form");
-    form.addEventListener("submit", handleFormSubmission);
-  });
+window.addEventListener("load", function () {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", handleFormSubmission);
+});
 
 
 
